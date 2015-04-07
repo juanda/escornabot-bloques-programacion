471819cb9db11f4defa14cde56569b6576106017
juanda <juandalibaba@gmail.com>
2015-04-07 08:04:55 +0200

    algunos defines más y ajuste del array programa para que comiencen en 0

--- programador/programador.ino
+++ programador/programador.ino
@@ -1,18 +1,18 @@
+/*
+ Programador por bloques para el escornabot.
+*/
+
+#define MIN_DIGITAL_OUT 4
+#define MAX_DIGITAL_OUT 13
+#define MIN_ANALOG_IN 0
+#define MAX_ANALOG_IN 5
+#define NUM_ANALOG_IN   MAX_ANALOG_IN - MIN_ANALOG_IN + 1
+#define NUM_DIGITAL_OUT MAX_DIGITAL_OUT - MIN_DIGITAL_OUT + 1 
+
 int btnReadProg    = 2;
 int leerPrograma   = 0;
-int minDigitalOut  = 3;
-int maxDigitalOut  = 13;
-int minAnalogIn    = 0;
-int maxAnalogIn    = 5;
 int timeStab       = 500; // tiempo que dejaremos pasar para que se
                           // estabilice las entradas analogicas
-
-// Desgraciadamente en C/C++ para declarar un array de tamaño constante
-// hay que saber en tiempo de compilacion el tamaño de este, por lo que
-// no podemos declarar las siguientes definiciones como variables.
-#define NUM_ANALOG_IN 6     // Debe ser maxAnalogIn - minAnalogIn + 1
-#define NUM_DIGITAL_OUT 11  // Debe ser maxDigitalOut - minDigitalOut + 1;      
-
 char programa[NUM_ANALOG_IN][NUM_DIGITAL_OUT];
 
 
@@ -21,7 +21,7 @@ void setup(){
   
   // Definimos los pines del 3 al 13 como de salida y colocamos 
   // 0 Volts en ellos
-  for(int pin = minDigitalOut; pin <= maxDigitalOut; pin++){
+  for(int pin = MIN_DIGITAL_OUT; pin <= MAX_DIGITAL_OUT; pin++){
     pinMode(pin, OUTPUT);
     digitalWrite(pin,LOW);
   }
@@ -36,48 +36,57 @@ void loop(){
   
   // Si el pulsador de lectura de programa es normalmente
   // cerrado hay que poner LOW, si es normalmente abierto
-  // HIGH. La cosa es que la condicion se cumple cuando se
+  // HIGH. La cosa es que la condicion se cumpla cuando se
   // ha pulsado el boton de lectura del programa 
-  if(leerPrograma == LOW){    
-    for(int pin = minDigitalOut; pin <= maxDigitalOut; pin++){
+  if(leerPrograma == HIGH){    
+    for(int pin = MIN_DIGITAL_OUT; pin <= MAX_DIGITAL_OUT; pin++){
       // Encendemos la salida digital dada por 'pin'
       nextOutputConfiguration(pin);
       delay(timeStab); // Esperamos un ratito
       // leemos todas las entradas analogicas
-      for(int a = minAnalogIn; a <= maxAnalogIn; a++){
-        programa[a][pin] = decodeVoltageLevel(analogRead(a));
-      }      
-    }    
-  }
-  
-  // Ahora lanzamos el programa al monitor serial. En su momento
-  // habra que hacerlo al bluetooth.
-  for(int a = minAnalogIn; a <= maxAnalogIn; a++){
-    for(int pin = minDigitalOut; pin <= maxDigitalOut; pin++){            
-        Serial.println(programa[a][pin]);
-      }      
+      for(int a = MIN_ANALOG_IN; a <= MAX_ANALOG_IN; a++){
+        programa[a-MIN_ANALOG_IN][pin-MIN_DIGITAL_OUT] = decodeVoltageLevel(analogRead(a));
+        Serial.print(a);
+        Serial.print("\t");
+        Serial.print(pin);
+        Serial.print("\t");
+        Serial.println(programa[a-MIN_ANALOG_IN][pin-MIN_DIGITAL_OUT]);
+      }
     }
+    // Ahora lanzamos el programa al monitor serial. En su momento
+    // habra que hacerlo al bluetooth.
+    for(int a = MIN_ANALOG_IN; a <= MAX_ANALOG_IN; a++){
+      for(int pin = MIN_DIGITAL_OUT; pin <= MAX_DIGITAL_OUT; pin++){               
+        Serial.println(programa[a-MIN_ANALOG_IN][pin-MIN_DIGITAL_OUT]);
+      }      
+    }       
+  }  
+
 }
 
 // Esta funcion coloca todos los pines digitales de salida a nivel
 // 0, a excepcion del pin dado por el argumento 'pin', que lo 
 // coloca a nivel alto
 void nextOutputConfiguration(int pin){
-  for(int i = minDigitalOut; i <= maxDigitalOut; i++){
+  for(int i = MIN_DIGITAL_OUT; i <= MAX_DIGITAL_OUT; i++){
     if(i == pin) digitalWrite(i, HIGH); else digitalWrite(i, LOW);
   }
 }
 
 // voltageLevel puede tomar valores comprendidos entre 0 y 1023
 char decodeVoltageLevel(int voltageLevel){
-  
+
   char result;
   
-  if(voltageLevel >= 0   && voltageLevel <= 204)  result = 'S'; // STOP
-  if(voltageLevel >= 205 && voltageLevel <= 408)  result = 'A'; // AVANZA
-  if(voltageLevel >= 409 && voltageLevel <= 612)  result = 'R'; // RETROCEDE
-  if(voltageLevel >= 613 && voltageLevel <= 816)  result = 'D'; // GIRO DERECHA
-  if(voltageLevel >= 817 && voltageLevel <= 1023) result = 'I'; // GIRO IZQUIERDA  
-  
+  if(voltageLevel >= 0  && voltageLevel <= 25)   result = 'S'; // STOP
+  if(voltageLevel >= 25 && voltageLevel <= 30)   result = 'A'; // AVANZA
+  if(voltageLevel >= 30 && voltageLevel <= 36)   result = 'R'; // RETROCEDE
+  if(voltageLevel >= 36 && voltageLevel <= 45)   result = 'D'; // GIRO DERECHA
+  if(voltageLevel >= 45 && voltageLevel <= 1023) result = 'I'; // GIRO IZQUIERDA  
+
+  Serial.print(voltageLevel);
+  Serial.print("\t");
+  Serial.print(result);
+  Serial.print("\t");
   return result;
 }
